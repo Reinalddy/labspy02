4823c9a24e0498a4f3b9f4b9c0f756905e57774b
reinalddy <reinalddyaldy183@gmail.com>
2021-11-15 21:07:53 +0700

    menambahkan folder

---
+++ kalkulator/main.py
@@ -0,0 +1,25 @@
+
+
+angka_pertama = float(input("masukan bilngan pertama : "))
+operator = (input("masukan operator : "))
+angka_kedua = float(input("masukab bilangan kedua : "))
+
+
+if operator == "/":
+    hasil = angka_pertama / angka_kedua
+    print ("hasil nya adalah = ", hasil)
+
+elif operator == "+" :
+    hasil = angka_pertama + angka_kedua
+    print ("hasil nya adalah : ", hasil)
+
+elif operator == "*" :
+    hasil = angka_pertama * angka_kedua
+    print ("hasil nya adalah =", hasil)
+
+elif operator == "-" :
+    hasil = angka_pertama - angka_kedua
+    print ("hasil nya adalah = ", hasil)
+
+
+
